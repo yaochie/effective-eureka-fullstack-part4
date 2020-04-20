45c49b4200ab3dafd5a114bd27ddaf1ebf8688e2
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-20 12:23:00 -0400

    refactor routing to separate module

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 
 const Blog = require('./models/blog')
@@ -18,23 +19,7 @@ mongoose.connect(
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 
