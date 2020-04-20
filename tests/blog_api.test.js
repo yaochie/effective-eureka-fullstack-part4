06e28a842dc5da801d9cb66975d3ef1a0582ab7a
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-20 18:41:50 -0400

    exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,7 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 const blogConsts = require('./blogs_for_test')
-
+const helper = require('./test_helper')
 const api = supertest(app)
 
 beforeEach(async () => {
@@ -31,6 +31,29 @@ test('all blogs have identifier id, which is unique', async () => {
   const response = await api.get('/api/blogs')
 
   response.body.forEach(blog => expect(blog.id).toBeDefined())
+})
+
+test('new blog is created', async () => {
+  const newBlog = {
+    title: 'New blogpost',
+    author: 'them',
+    url: 'does.not.exist',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // check that length has increased
+  const newBlogs = await helper.blogsInDb()
+  expect(newBlogs).toHaveLength(blogConsts.blogs.length + 1)
+
+  // check that new blog exists
+  expect(newBlogs.map(blog => blog.title)).toContain(newBlog.title)
+  expect(newBlogs.map(blog => blog.author)).toContain(newBlog.author)
 })
 
 
