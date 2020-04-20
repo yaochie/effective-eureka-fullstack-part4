46874bb3ecd6101f38e58198334a9bce5214ac13
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-20 12:17:44 -0400

    refactor Blog schema to separate module

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(
   config.MONGODB_URI,
